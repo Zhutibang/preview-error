3e932c5c731f740edd913ddfc5a9dedbfb0c2b80
Jayin Ton <tonjayin@gmail.com>
2015-11-22 20:55:30 +0800

    :cd: Update at Sun Nov 22 20:55:30 CST 2015

--- script/rm-wechat-info.js
+++ script/rm-wechat-info.js
@@ -1,7 +1,7 @@
-/**
- * 移除微信文章的标题, 发布时间 作者信息
- */
 (function(){
+    /**
+     * 移除微信文章的标题, 发布时间 作者信息
+     */
     document.getElementById('activity-name').innerHTML = '';
     document.getElementsByClassName('rich_media_meta_list')[0].innerHTML = '';
 })();
